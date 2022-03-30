cb24fcff31bbad3ab5d23300e014295036594262
bgblackley1 <bgblackley1@outlook.com>
2022-03-30 15:06:05 +0100

    Pythonanywhere update

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sq_z*m=yr)aiu$9lyzf@)&-!o(yjs=)9igwhuco$d7@a3jed=@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bgbla.pythonanywhere.com']
 
 
 # Application definition
